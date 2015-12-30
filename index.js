12a80606dbdabdf462da003cad01ec168347e4e1
Grant DeLozier <grantdelozier@gmail.com>
2015-12-30 12:19:31 -0600

    Fix bug that prevented annotated column from updating correctly for location page

--- index.js
+++ index.js
@@ -185,14 +185,26 @@ app.post('/annotate/payer-annotate', function(request, response) {
 								}
 								else{
 									console.log("Saved Entry by ", request.session.username);
-									client.query("UPDATE article_texts SET annotated = $1 WHERE id = $2;", [1, request.body.vol], function(err4, result){
-										if (err4){
-											response.send("Error:"+ err4)
-										}
-										else{
-											response.send("Saving Successful on vol " + request.body.vol)
-										}
-									});
+									if (request.body.page == 'payer'){
+										client.query("UPDATE article_texts SET annotated = $1 WHERE id = $2;", [1, request.body.vol], function(err4, result){
+											if (err4){
+												response.send("Error:"+ err4)
+											}
+											else{
+												response.send("Saving Successful on vol " + request.body.vol)
+											}
+										});
+									}
+									else if (request.body.page == 'location'){
+										client.query("UPDATE article_texts_location SET annotated = $1 WHERE id = $2;", [1, request.body.vol], function(err4, result){
+											if (err4){
+												response.send("Error:"+ err4)
+											}
+											else{
+												response.send("Saving Successful on vol " + request.body.vol)
+											}
+										});
+									};
 								}
 							});
 						}
